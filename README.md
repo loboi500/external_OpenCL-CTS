9341d9ad154cda24a4631b3266bebf8251b2885a
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 22:24:29 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# OpenCL-CTS [![Build Status](https://api.travis-ci.org/KhronosGroup/OpenCL-CTS.svg?branch=master)](https://travis-ci.org/KhronosGroup/OpenCL-CTS/branches)
-The OpenCL Conformance Tests
+version https://git-lfs.github.com/spec/v1
+oid sha256:c22fb4e4d1c2dad2f876a0da268871f8afe7bef4fe4b73d75e69bab39ce64e08
+size 185
--- check-format.sh
+++ check-format.sh
@@ -1,17 +1,3 @@
-#!/usr/bin/env bash
-
-# Arg used to specify non-'origin/master' comparison branch
-ORIGIN_BRANCH=${1:-"origin/master"}
-CLANG_BINARY=${2:-"`which clang-format-9`"}
-
-# Run git-clang-format to check for violations
-CLANG_FORMAT_OUTPUT=$(git-clang-format --diff $ORIGIN_BRANCH --extensions c,cpp,h,hpp --binary $CLANG_BINARY)
-
-# Check for no-ops
-grep '^no modified files to format$' <<<"$CLANG_FORMAT_OUTPUT" && exit 0
-grep '^clang-format did not modify any files$' <<<"$CLANG_FORMAT_OUTPUT" && exit 0
-
-# Dump formatting diff and signal failure
-echo -e "\n==== FORMATTING VIOLATIONS DETECTED ====\n"
-echo "$CLANG_FORMAT_OUTPUT"
-exit 1
+version https://git-lfs.github.com/spec/v1
+oid sha256:4951af07bd23f237192f77aeb35e3507abcc90d8b9f46bccc8f0129a8687cf53
+size 628
